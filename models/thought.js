e85f81841cbc30a2c58a29b2f611c4a557a9ced1
Matt Bianco <matthewbianco@protonmail.com>
2021-11-03 20:24:42 -0400

    added server.js

--- models/thought.js
+++ models/thought.js
@@ -1,21 +1,44 @@
 const { Schema } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const reactionSchema = new Schema({
+    reactionId: {
+
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: dateFormat,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+})
 
 const thoughtSchema = new Schema ({
-
     thoughtText: {
         type: String,
         Required: true,
         min: 1,
         max: 280
     },
-
     createdAt: {
         type: dateFormat,
         default: Date.now,
         get: createdAtVal => dateFormat(createdAtVal)
 
-    }
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reaction: [reactionSchema]
 
 })
